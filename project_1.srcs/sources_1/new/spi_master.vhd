6457f50635f5b09631b2208eed926b8ccfe97ae5
275793-pwr <275793@student.pwr.edu.pl>
2025-06-18 12:10:58 +0200

    change spi

--- project_1.srcs/sources_1/new/spi_master.vhd
+++ project_1.srcs/sources_1/new/spi_master.vhd
@@ -18,11 +18,11 @@ ARCHITECTURE Behavioral OF spi_master IS
 
     TYPE state_type IS (IDLE, START_TX, SEND_BIT, END_TX);
     SIGNAL current_state, next_state : state_type := IDLE;
-    SIGNAL bit_count : INTEGER RANGE 0 TO 12 := 0;
+    SIGNAL bit_count : INTEGER RANGE 0 TO 16 := 0;
     SIGNAL spi_sck_int : STD_LOGIC := '0';
     SIGNAL spi_ss_int : STD_LOGIC := '1';
     SIGNAL busy_int : STD_LOGIC := '0';
-    SIGNAL miso_buffer : STD_LOGIC_VECTOR(11 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL miso_buffer : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
     SIGNAL clk_div_counter : INTEGER RANGE 0 TO 250 := 0; -- Counter for clock division
 
 BEGIN
@@ -30,7 +30,7 @@ BEGIN
     sck <= spi_sck_int;
     ss <= spi_ss_int;
     busy <= busy_int;
-    miso_data_out <= miso_buffer;
+    miso_data_out <= miso_buffer(12 downto 1);
 
     PROCESS (clk)
     BEGIN
@@ -64,7 +64,7 @@ BEGIN
                             spi_sck_int <= NOT spi_sck_int;
                             -- IF spi_sck_int = '1' THEN -- Sample MISO on falling edge
                             IF spi_sck_int = '0' THEN -- Sample MISO on rising edge
-                                miso_buffer(11 - bit_count) <= miso;
+                                miso_buffer(15 - bit_count) <= miso;
                                 bit_count <= bit_count + 1;
                             END IF;
                         END IF;
